2483e057566c18117973ba4ebe4825b7a927336a
Stanislav Angelov <stanislavangelov1995@gmail.com>
2017-12-14 18:06:40 +0100

    JobStoreInitializer working with dummy data on db creation

--- Semester1Project/Dal/JobStoreInitializer.cs
+++ Semester1Project/Dal/JobStoreInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Semester1Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,16 @@ namespace Semester1Project.Dal
     {
         protected override void Seed(JobStoreContext context)
         {
+            List<User> Users = new List<User>();
+            List<Job> Jobs = new List<Job>();
+
+            Users.Add(new User() { FullName = "John Smith", Email = "johnsmithcorp@jscorp.de", JobCount = 5, PhoneNum = "555123123", UserName = "johnnyboy" , Pass= "qwe"});
+            Jobs.Add(new Job() { Date = new DateTime(2015, 11, 5) , Description = "Walk My dog every day for a month" , ExpectedHours=2, JobCreator=Users.FirstOrDefault() , MaxWorkers=1 , Location="Aarhus", Payment=0 , Title = "Walk my dog"  } );
+            Jobs.Add(new Job() { Date = new DateTime(2015, 11, 5), Description = "Help me with my gardening", ExpectedHours = 2, JobCreator = Users.FirstOrDefault(), MaxWorkers = 1, Location = "Aarhus", Payment = 0, Title = "Help cutting grass" });
+            Jobs.Add(new Job() { Date = new DateTime(2015, 11, 5), Description = "Wash my car", ExpectedHours = 2, JobCreator = Users.FirstOrDefault(), MaxWorkers = 1, Location = "Aarhus", Payment = 0, Title = "Help me wash my car inside and out" });
+            context.Users.Add(Users.FirstOrDefault());
+            Jobs.ForEach(x => context.Jobs.Add(x));
+            context.SaveChanges();
         }
     }
 }
