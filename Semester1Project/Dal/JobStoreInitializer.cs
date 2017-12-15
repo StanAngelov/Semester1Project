45f95dd818a480ab285f09ac3d8beb669e8c2bfc
Krisztian Nagy <kns@ditaexchange.com>
2017-12-15 08:04:20 +0100

    initializer expanded

--- Semester1Project/Dal/JobStoreInitializer.cs
+++ Semester1Project/Dal/JobStoreInitializer.cs
@@ -13,10 +13,24 @@ namespace Semester1Project.Dal
             List<User> Users = new List<User>();
             List<Job> Jobs = new List<Job>();
 
-            Users.Add(new User() { FullName = "John Smith", Email = "johnsmithcorp@jscorp.de", JobCount = 5, PhoneNum = "555123123", UserName = "johnnyboy" , Pass= "qwe"});
-            Jobs.Add(new Job() { Date = new DateTime(2019, 11, 5) , Description = "Walk My dog every day for a month" , ExpectedHours=2, JobCreator=Users.FirstOrDefault() , MaxWorkers=1 , Location="Aarhus", Payment=400 , Title = "Walk my dog"  } );
-            Jobs.Add(new Job() { Date = new DateTime(2019, 11, 5), Description = "Help me with my gardening", ExpectedHours = 2, JobCreator = Users.FirstOrDefault(), MaxWorkers = 1, Location = "Aarhus", Payment = 150, Title = "Help cutting grass" });
-            Jobs.Add(new Job() { Date = new DateTime(2019, 11, 5), Description = "Wash my car", ExpectedHours = 2, JobCreator = Users.FirstOrDefault(), MaxWorkers = 1, Location = "Aarhus", Payment = 200, Title = "Help me wash my car inside and out" });
+            User user1 = new User() { FullName = "John Smith", Email = "johnsmithcorp@jscorp.de", JobCount = 0, PhoneNum = "555123123", UserName = "johnnyboy", Pass = "qwe" };
+            User user2 = new User() { FullName = "Johnny Test", Email = "jhonnytest@whatever,com", JobCount = 0, PhoneNum = "444123456", UserName = "jtest", Pass = "jtest" };
+            User user3 = new User() { FullName = "James Johnson", Email = "jjs@johnsons.org", JobCount = 0, PhoneNum = "921555123", UserName = "jjs", Pass = "jjs" };
+            User user4 = new User() { FullName = "Jane Doe", Email = "janedoe@doecorp.com", JobCount = 0, PhoneNum = "341234567", UserName = "jane", Pass = "jane" };
+
+            Users.Add(user1);
+            Users.Add(user2);
+            Users.Add(user3);
+            Users.Add(user4);
+
+            Job job1 = new Job() { Date = new DateTime(2019, 11, 5), Description = "Walk My dog every day for a month", ExpectedHours = 2, JobCreator = user2, MaxWorkers = 1, Location = "Aarhus", Payment = 400, Title = "Walk my dog" };
+            Job job2 = new Job() { Date = new DateTime(2019, 11, 5), Description = "Help me with my gardening", ExpectedHours = 2, JobCreator = user3, MaxWorkers = 1, Location = "Aarhus", Payment = 150, Title = "Help cutting grass" };
+            Job job3 = new Job() { Date = new DateTime(2019, 11, 5), Description = "Wash my car", ExpectedHours = 2, JobCreator = user1, MaxWorkers = 1, Location = "Aarhus", Payment = 200, Title = "Help me wash my car inside and out" };
+
+            Jobs.Add(job1);
+            Jobs.Add(job2);
+            Jobs.Add(job3);
+
             context.Users.Add(Users.FirstOrDefault());
             Jobs.ForEach(x => context.Jobs.Add(x));
             context.SaveChanges();
